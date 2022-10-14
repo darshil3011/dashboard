94e5bf16ab3c3eb76a6ab4be625003e6c75b09e2
Darshil Modi <45987056+darshil3011@users.noreply.github.com>
2022-10-14 12:49:29 +0530

    Update data_visualization_app.py

--- data_visualization_app.py
+++ data_visualization_app.py
@@ -129,9 +129,7 @@ if chart_select == 'Scatterplots':
     st.sidebar.subheader("Scatterplot Settings")
     st.subheader("Scatterplot")
     
-    st.markdown("A scatter plot (aka scatter chart, scatter graph) uses dots to represent values for two different numeric variables. 
-                The position of each dot on the horizontal and vertical axis indicates values for an individual data point. 
-                Scatter plots are used to observe relationships between variables.")
+    st.markdown("A scatter plot uses dots to represent values for two different numeric variables. The position of each dot on the horizontal and vertical axis indicates values for an individual data point. Scatter plots are used to observe relationships between variables.")
     
     st.subheader("When to use scatterplots")            
     st.markdown("A scatter plot can also be useful for identifying other patterns in data. 
